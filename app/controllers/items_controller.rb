1325ad696de4760aec3facded2091a13e6b08110
Brandon Caudillo <btcaudillo@gmail.com>
2023-03-21 19:30:17 -0600

    commit tree d461540fc00b242dd82761ae65009047fb6f472a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found_error
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(params.permit(:name, :description, :price))
+    render json: item, status: :created
+  end
+
+  private 
+
+  def user_not_found_error
+    render json: {error: "nah bro"}, status: :not_found
   end
 
 end
